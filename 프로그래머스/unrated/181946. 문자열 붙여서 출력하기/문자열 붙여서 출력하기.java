f736f765fd68447abdd52136e532c294c35fa060
hjYoon <101798354+hjYoon66@users.noreply.github.com>
2023-08-07 18:21:28 +0900

    [unrated] Title: 문자열 붙여서 출력하기, Time: 144.18 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -6,6 +6,6 @@ public class Solution {
         String a = sc.next();
         String b = sc.next();
         
-        System.out.println((a+b).trim());
+        System.out.println(a.concat(b));
     }
 }
