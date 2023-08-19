21b92a7b330b3f18281fdd8f4e47f9953cdca609
hjYoon <101798354+hjYoon66@users.noreply.github.com>
2023-08-19 22:16:28 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.51 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -2,60 +2,28 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        int answer = 0;
-        int[] nums=new int[4];
-        nums[0]=a;
-        nums[1]=b;
-        nums[2]=c;
-        nums[3]=d;
-        
-        int multi=1;
-        
-        Arrays.sort(nums);//오름차순 정렬
-        int[] dice=new int[6];
-        for(int i=0;i<4;i++){
-            dice[nums[i]-1]++;
+
+        int[] dice = { a, b, c, d };
+        Arrays.sort(dice);
+
+        int ans = 0;
+
+        if (dice[0] == dice[3]) {
+            ans = 1111 * dice[3];
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            ans = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            ans = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+        } else if (dice[0] == dice[1]) {
+            ans = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            ans = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            ans = dice[0] * dice[1];
+        } else {
+            ans = dice[0];
         }
-        
-        for(int i=0;i<6;i++){
-            if(dice[i]==4){
-                answer=1111*(i+1);
-                break;
-            }
-            else if(dice[i]==3){
-                for(int j=0;j<6;j++){
-                    if(dice[j]==1){
-                        answer=(10*(i+1)+(j+1))*(10*(i+1)+(j+1));
-                        break;
-                    }
-                }
-            }
-            else if(dice[i]==2){
-               for(int j=0;j<6;j++){
-                    if(dice[j]==2){
-                        if(i==j){
-                            continue;
-                        }
-                        else{
-                            answer=(i+1+j+1)*((i+1)-(j+1));
-                            break;
-                        }
-                        
-                    }
-                   else if(dice[j]==1){
-                        multi=multi*(j+1);
-                   }
-                }
-                if(multi!=1){
-                    answer=multi;
-                }
-            }
-            if(nums[0]!=nums[1] && nums[1]!=nums[2] && nums[2]!=nums[3]){
-                answer=nums[0];
-            }
-                
-        }
-        
-        return answer;
+
+        return ans;
     }
 }
