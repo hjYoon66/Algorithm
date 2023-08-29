b2ce77f6a9a2af7653d4b159e6771c8cb9d77e5c
hjYoon <101798354+hjYoon66@users.noreply.github.com>
2023-08-29 15:32:55 +0900

    [unrated] Title: 글자 지우기, Time: 0.04 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -1,10 +1,16 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        StringBuilder sb = new StringBuilder(my_string);
-        for(int indice : indices)
-            sb.setCharAt(indice,' ');
-        answer = sb.toString().replace(" ","");
+        boolean[] flags = new boolean[my_string.length()];
+        for (int i : indices) {
+            flags[i] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < flags.length; i++) {
+            if (!flags[i])
+                sb.append(my_string.charAt(i));
+        }
+        answer = sb.toString();
         return answer;
     }
 }
