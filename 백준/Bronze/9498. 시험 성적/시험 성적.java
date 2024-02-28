d9c995f3ccef174c1fb7b13a94d78098ac3eec14
hjYoon <101798354+hjYoon66@users.noreply.github.com>
2024-02-28 15:24:16 +0900

    [Bronze V] Title: 시험 성적, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int score = in.nextInt();
+        System.out.println((score >= 90) ? "A" : (score >= 80) ? "B" : (score >= 70) ? "C" : (score >= 60) ? "D" : "F");
+    }
+}
