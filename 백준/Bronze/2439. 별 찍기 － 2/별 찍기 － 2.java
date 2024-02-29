9cc19333959f6b31fa2aed0c0a03c927609d897b
hjYoon <101798354+hjYoon66@users.noreply.github.com>
2024-02-29 10:48:56 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 376 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < i; k++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
