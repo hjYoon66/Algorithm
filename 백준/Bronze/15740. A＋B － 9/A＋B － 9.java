b8b7c3f7a67af2604e4ce386b40bafe0aeaf326f
hjYoon <101798354+hjYoon66@users.noreply.github.com>
2025-01-22 20:03:06 +0900

    [Bronze V] Title: A+B - 9, Time: 212 ms, Memory: 17236 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -1,13 +1,13 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-
-        System.out.println(a + b);
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        System.out.println(a.add(b));
     }
 }
